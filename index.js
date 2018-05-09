d1f1dc91e5e6d01581fb075dfe0bb8dbf9346ae3
Cameron Cooper <cameron.cooper@dosh.cash>
2018-05-09 16:36:53 -0500

    hook stderr and etdout on console

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const jq = require('node-jq');
 class Pretty {
   constructor(serverless, options) {
     const filter = (((serverless || {}).service || {}).custom || {}).jsonOutputFilter || '.';
-    process.stdout.write = (function (write) {
+    const hook = function (write) {
       return function (string, encoding, fd) {
         jq.run(filter, string, { input: 'string', sort: true, color: true })
           .then((output) => {
-            write.apply(process.stdout, [output+'\n', encoding, fd]);
+            write.apply(process.stdout, [output + '\n', encoding, fd]);
           })
           .catch((err) => {
             write.apply(process.stdout, arguments);
           })
       }
-    })(process.stdout.write);
+    };
+
+    console._stdout.write = hook(console._stdout.write)
+    console._stderr.write = hook(console._stderr.write)
+    process.stdout.write = hook(process.stdout.write);
   }
 }
 
